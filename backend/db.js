a70cee8e5a1dca238e69edc36205362572cf7f29
Znoy <75539409+Znoy108x@users.noreply.github.com>
2022-09-10 14:45:01 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
-const db_name = "google-notes"
-const mongoose_url = `mongodb+srv://abhay:abhay@cluster0.i8prg.mongodb.net/${db_name}?retryWrites=true&w=majority`
+const db_name = "DB_NAME"
+const mongoose_url = "DB_URL"
 const connectToMongoose = () =>{
     mongoose.connect(mongoose_url , () =>{
         console.log("Connected to the Data Base 👌")
